cccb4214426ec6973b23f740f5ebc00369b35690
RohAn Raj <ml.rohan.raj.083@gmail.com>
2026-08-31 20:36:40 +0530

    Time: 788 ms (7.89%) | Memory: 259.9 MB (6.88%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -1,24 +1,27 @@
 class Solution {
-
     public int countPrimes(int n) {
-        if(n<=1) return 0;
-        int count = 0;
-        boolean ans [] = new boolean[n];
-        Arrays.fill(ans, true);
-
-        for(int i=2;(long)i*i<n; i++){
-            
-            if(ans[i]){
-                for(int j=i*2; j<n; j+=i){ // anything smaller then i*i is already marked false by smaller factor
-                    ans[j] = false;
+        if(n<=2){
+            return 0;
+        }
+        return PrimeSeive(n);
+    }
+    public static int PrimeSeive(int n ){
+        int[] prime = new int[n];
+        prime[0]= 1; // yeh prime number nhi h 
+        prime[1] = 1; //yeh prime number nhi h 
+        for(int i = 2;i*i<=prime.length;i++){
+            if(prime[i]==0){
+                for(int j = 2;i*j<prime.length;j++){
+                    prime[i*j]=1;
                 }
             }
         }
-
-        for(int i=2; i<n; i++){
-            if(ans[i]) count++;
+        int c = 0;
+        for(int i = 2;i<prime.length;i++){
+            if(prime[i]==0){
+                c++;
+            }
         }
-
-        return count;
+        return c ;
     }
 }
